de73878624e75d8acb7c12ddf0002b47cccd4020
Alberto Redondo <albertoredondoalvarez@gmail.com>
2024-12-28 17:52:06 +0000

    Kata4

---
+++ src/main/java/software/ulpgc/kata4/io/FileMovieReader.java
@@ -0,0 +1,28 @@
+package software.ulpgc.kata4.io;
+
+import software.ulpgc.kata4.model.Movie;
+
+import java.io.*;
+
+public class FileMovieReader implements MovieReader, AutoCloseable{
+    private final MovieDeserializer deserializer;
+    private BufferedReader reader;
+
+    public FileMovieReader(MovieDeserializer deserializer, File file) throws IOException {
+        this.deserializer = deserializer;
+        this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+    }
+
+    @Override
+    public Movie read() throws IOException {
+        return deserialize(reader.readLine());
+    }
+    private Movie deserialize(String line){
+        return line != null ? deserializer.deserialize(line) : null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.reader.close();
+    }
+}
